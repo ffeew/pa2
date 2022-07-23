0d7e210efcf8f798fa31450e7a01e2deae03b3d1
Daniel Koh <aikhong_koh@mymail.sutd.edu.sg>
2022-07-24 07:44:37 +0800

    chained authentication and ftp together

--- source/ClientWithSecurityAP.py
+++ source/ClientWithSecurityAP.py
@@ -49,86 +49,82 @@ def main(args):
     # Connect to server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((server_address, port))
-        print("Connected")
+        print("Connected\n")
 
         while True:
-            mode = input("Choose a mode:\n0: Send file\n3: Begin Authentication Protocol\n")
+            # authenticate first before proceeding with FTP
 
-            match mode:
-                case "0":
-                    filename = input("Enter a filename to send (enter -1 to exit):")
-                    while filename != "-1" and (not pathlib.Path(filename).is_file()):
-                        filename = input("Invalid filename. Please try again:")
-                    if filename == "-1":
-                        s.sendall(convert_int_to_bytes(2))
-                        break
+            # hash the auth_message before sending it
+            auth_message = hashes.Hash(hashes.SHA256())
+            # create a random message each time to prevent 
+            # replay attacks if the attacker knows the auth message
+            nonce = secrets.token_bytes(32) # 256 bit nonce
+            # hash the auth message to further protect it
+            # in case the 
+            auth_message.update(nonce)
+            auth_message = auth_message.finalize()
 
-                    filename_bytes = bytes(filename, encoding="utf8")
+            auth_message_size = len(auth_message)
+            s.sendall(convert_int_to_bytes(3))
+            s.sendall(convert_int_to_bytes(auth_message_size))
+            s.sendall(auth_message)
+            signed_auth_msg_length = s.recv(8)
+            signed_auth_msg = s.recv(convert_bytes_to_int(signed_auth_msg_length))
+            signed_cert_length = s.recv(8)
+            signed_cert = s.recv(convert_bytes_to_int(signed_cert_length))
 
-                    # Send the filename
-                    s.sendall(convert_int_to_bytes(0))
-                    s.sendall(convert_int_to_bytes(len(filename_bytes)))
-                    s.sendall(filename_bytes)
+            # parse the cert received
+            server_cert = x509.load_pem_x509_certificate(
+                data=signed_cert, backend=default_backend()
+            )
 
-                    # Send the file
-                    with open(filename, mode="rb") as fp:
-                        data = fp.read()
-                        s.sendall(convert_int_to_bytes(1))
-                        s.sendall(convert_int_to_bytes(len(data)))
-                        s.sendall(data)
+            # verify if the signed cert from server is authentic
+            ca_public_key.verify(
+                signature=server_cert.signature, # signature bytes to  verify
+                data=server_cert.tbs_certificate_bytes, # certificate data bytes that was signed by CA
+                padding=padding.PKCS1v15(), # padding used by CA bot to sign the the server's csr
+                algorithm=server_cert.signature_hash_algorithm,
+            )
+            
+            # check if the server_cert is valid
+            assert server_cert.not_valid_before <= datetime.utcnow() <= server_cert.not_valid_after
 
+            # obtain the server cert's public key
+            public_key = server_cert.public_key()
 
-                case "3":
-                    # hash the auth_message before sending it
-                    auth_message = hashes.Hash(hashes.SHA256())
-                    # create a random message each time to prevent brute force
-                    nonce = secrets.token_bytes(32) # 256 bit nonce
-                    auth_message.update(nonce)
-                    auth_message = auth_message.finalize()
+            # check that the server signed the message
+            # signed_auth_msg should be == auth_message when decrypted with the public key
+            public_key.verify(
+                signed_auth_msg,
+                auth_message,
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH,
+                ),
+                hashes.SHA256(),
+            )
+            print("Authenticated\n")
 
-                    auth_message_size = len(auth_message)
-                    s.sendall(convert_int_to_bytes(3))
-                    s.sendall(convert_int_to_bytes(auth_message_size))
-                    s.sendall(auth_message)
-                    signed_auth_msg_length = s.recv(8)
-                    signed_auth_msg = s.recv(convert_bytes_to_int(signed_auth_msg_length))
-                    signed_cert_length = s.recv(8)
-                    signed_cert = s.recv(convert_bytes_to_int(signed_cert_length))
+            filename = input("Enter a filename to send (enter -1 to exit):")
+            while filename != "-1" and (not pathlib.Path(filename).is_file()):
+                filename = input("Invalid filename. Please try again:")
+            if filename == "-1":
+                s.sendall(convert_int_to_bytes(2))
+                break
 
-                    # parse the cert received
-                    server_cert = x509.load_pem_x509_certificate(
-                        data=signed_cert, backend=default_backend()
-                    )
+            filename_bytes = bytes(filename, encoding="utf8")
 
-                    # verify if the signed cert from server is authentic
-                    ca_public_key.verify(
-                        signature=server_cert.signature, # signature bytes to  verify
-                        data=server_cert.tbs_certificate_bytes, # certificate data bytes that was signed by CA
-                        padding=padding.PKCS1v15(), # padding used by CA bot to sign the the server's csr
-                        algorithm=server_cert.signature_hash_algorithm,
-                    )
-                    
-                    # check if the server_cert is valid
-                    assert server_cert.not_valid_before <= datetime.utcnow() <= server_cert.not_valid_after
+            # Send the filename
+            s.sendall(convert_int_to_bytes(0))
+            s.sendall(convert_int_to_bytes(len(filename_bytes)))
+            s.sendall(filename_bytes)
 
-                    # obtain the server cert's public key
-                    public_key = server_cert.public_key()
-
-                    # check that the server signed the message
-                    # signed_auth_msg should be == auth_message when decrypted with the public key
-                    public_key.verify(
-                        signed_auth_msg,
-                        auth_message,
-                        padding.PSS(
-                            mgf=padding.MGF1(hashes.SHA256()),
-                            salt_length=padding.PSS.MAX_LENGTH,
-                        ),
-                        hashes.SHA256(),
-                    )
-                    print("Authenticated\n")
-
-
-
+            # Send the file
+            with open(filename, mode="rb") as fp:
+                data = fp.read()
+                s.sendall(convert_int_to_bytes(1))
+                s.sendall(convert_int_to_bytes(len(data)))
+                s.sendall(data)
 
         # Close the connection
         s.sendall(convert_int_to_bytes(2))
